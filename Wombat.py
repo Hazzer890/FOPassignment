66115f934998dcf6fc2ff12c309e56b042e2cf2a
Hazzer890 <harrycassidy@westnet.com>
2021-05-13 15:21:21 +0800

    Pathfinding around the lake + colour adjustment

--- Wombat.py
+++ Wombat.py
@@ -2,6 +2,7 @@
 # ChosenAnimals.py -  Contains the Classes for the chosen simulated animals
 #
 
+from random import randint
 from Animal import * 
 
 class wombat(animal):
@@ -19,18 +20,22 @@ class wombat(animal):
             if abs((self.foodCood[0]-self.posn[0])) == 0 and abs((self.foodCood[1]-self.posn[1])) == 0:
                 self.food = True
             elif abs((self.foodCood[0]-self.posn[0])) == 0:
-                self.posn[0] = self.posn[0] + 1
+                self.posn[0] = self.posn[0] + random.randint(-1,1)
             elif abs((self.foodCood[1]-self.posn[1])) == 0:
-                self.posn[1] = self.posn[1] + 1
+                self.posn[1] = self.posn[1] + random.randint(-1,1)
             else:
                 self.posn[0] = self.posn[0] + ((self.foodCood[0]-self.posn[0])/abs((self.foodCood[0]-self.posn[0])))
                 self.posn[1] = self.posn[1] + ((self.foodCood[1]-self.posn[1])/abs((self.foodCood[1]-self.posn[1])))
+                if (self.baseArea[int(self.posn[0]), int(self.posn[1])] == 1):
+                    self.posn[0] = self.posn[0] - ((self.foodCood[0]-self.posn[0])/abs((self.foodCood[0]-self.posn[0])))
+                    self.posn[1] = self.posn[1] - ((self.foodCood[1]-self.posn[1])/abs((self.foodCood[1]-self.posn[1])))
         else:
+            #approach home
             if abs((self.homeX-self.posn[0])) == 0 and abs((self.homeY-self.posn[1])) == 0:
                 pass
             elif abs((self.homeX-self.posn[0])) == 0 or abs((self.homeY-self.posn[1])) == 0:
-                self.posn[0] = self.posn[0] + 1
-                self.posn[1] = self.posn[1] + 1
+                self.posn[0] = self.posn[0] + random.randint(-1,1)
+                self.posn[1] = self.posn[1] + random.randint(-1,1)
             else:
                 self.posn[0] = self.posn[0] + ((self.homeX-self.posn[0])/abs((self.homeX-self.posn[0])))
                 self.posn[1] = self.posn[1] + ((self.homeY-self.posn[1])/abs((self.homeY-self.posn[1])))
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import pygame, sys
 import random
 import time
 from pygame.locals import *
-from ChosenAnimals import *
+from Wombat import *
 from Animal import *
 from SimulationBase import *
 
 # Variables
+numWom = 10
 dimX = 700
 dimY = 700
 Animal1 = []
@@ -21,9 +22,12 @@ FPS = 24
 FramePerSec = pygame.time.Clock()
 
 # colour objects
-WATERC  = (42, 68, 148)
+WATERC  = (42, 99, 148)
 FOODC   = (238, 185, 2)
-BACKGC = (62, 86, 65)
+BACKGC = (62, 103, 51)
+WOMBATC = (49, 42, 33)
+PREDATORC = (157, 6, 26)
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
@@ -33,9 +37,13 @@ DISPLAYSURF.fill(BACKGC)
 pygame.display.set_caption("Simulation")
 
 # Init Animals
-for i in range(15):
-    randX = random.randint(0,dimX-1)
-    randY = random.randint(0,dimY-1)
+for i in range(numWom):
+    yes = True
+    while yes:
+        randX = random.randint(0,dimX-1)
+        randY = random.randint(0,dimY-1)
+        if baseArea[randX,randY] != 1:
+            yes = False
     Animal1.append(wombat(randX, randY, dimX, dimY, baseArea))
 
 # Generate area
@@ -63,6 +71,6 @@ while True:
         lani = Animal1[i].pos()
         pygame.draw.rect(DISPLAYSURF, BACKGC, (lani[0], lani[1], 7, 7), 2)
         ani = Animal1[i].move()
-        pygame.draw.rect(DISPLAYSURF, BLACK, (ani[0], ani[1], 7, 7), 2)
+        pygame.draw.rect(DISPLAYSURF, WOMBATC, (ani[0], ani[1], 7, 7), 2)
 
     FramePerSec.tick(FPS)
